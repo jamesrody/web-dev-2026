f103c2ce7df39c5f330b47b490c5498afb68c037
jamesrody <134702414+jamesrody@users.noreply.github.com>
2026-03-19 20:01:54 -0700

    bucket!

--- nuxt-app/app/app.vue
+++ nuxt-app/app/app.vue
@@ -1,13 +1,10 @@
 <template>
-  <section>
-    <div>
-      Column1
-    </div>
-    <div>
-      Column2
-    </div>
-    <div>
-      Column3
-    </div>
+  <section class="flex flex-row justify-between gap-4 max-w-6xl mx-auto">
+    <Bucket title="pie" className="bg-red-100">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt aliquam ab maiores ipsam! Soluta iste nostrum
+      architecto sunt atque facilis maiores, et quaerat totam sit rem. Dignissimos autem possimus quis.
+    </Bucket>
+    <Bucket title="Column 2" />
+    <Bucket title="Column 2" className="bg-green-400" />
   </section>
 </template>
